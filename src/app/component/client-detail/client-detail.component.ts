fa560f594b95b82537c3483ae99011f2a99293bc
Oda <oda.schneider@gmx.de>
2022-11-17 14:48:35 +0100

    user-details added

--- src/app/component/client-detail/client-detail.component.ts
+++ src/app/component/client-detail/client-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from 'src/models/client.class';
 
 @Component({
   selector: 'app-client-detail',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientDetailComponent implements OnInit {
 
-  constructor() { }
+  clientId = '';
+  client: Client = new Client();
+
+  allClients = [];
+
+  constructor(    
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+    private dialog: MatDialog,
+    public router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      this.clientId = paramMap.get('id');
+      this.getClient();
+    })
+  }
+
+
+  getClient() {
+    this.firestore
+      .collection('clients')
+      .doc(this.clientId)
+      .valueChanges()
+      .subscribe((client: any) => {
+        this.client = new Client(client);
+      })
+  }
+
+
+  deleteClient() {
+    this
+      .firestore
+      .collection('contact')
+      .doc(this.clientId)
+      .delete();
+    this.router.navigate(['mainpage/clients']);
+  }
+
+
+  editAddress(){
+    
   }
 
 }
